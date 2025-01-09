155dd08e50a2bbcce477702cd7d50dd3a778f5c1
mamboespacio <emadobao+1@gmail.com>
2025-01-09 20:09:04 -0300

    Allow new fields for User model

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default ({ env }) => ({
+  // ...
+  "users-permissions": {
+    config: {
+      register: {
+        allowedFields: ["fullName", "dni"],
+      },
+    },
+  },
+  // ...
+});
